6fef35be84e1215d049f220c81ab6c31ec0dfa64
Vanessa Suthat <vanessasuthat@vanessas-mbp-2.mynet>
2020-04-09 22:51:15 +0200

    Require_once header and footer.

--- contact.php
+++ contact.php
@@ -1,27 +1,15 @@
 <?php
 
+require_once "header.php";
+ require_once "style_contact.css";
 ?>
 
-<!-- test -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style_contact.css">
-    <title>Document</title>
-</head>
-<body>
-    
-<!-- test -->
 
+<div class="container">
 <div id="contact-form">
 	<div>
 		<h4>Have a question or just want to get in touch?<br> Fyll out this form and let us know.</h4> 
 	</div>
-		<!-- <p id="failure">Oopsie...message not sent.</p>  
-		<p id="success">Thank you! We will get back to you shortly.</p> -->
 
 		   <form method="post" action="#">
 			<div>
@@ -33,7 +21,7 @@
 			<div>
 		      <label for="email">
 		      	<span class="required">Email: *</span>
-		      	<input type="email" id="email" name="email" value="" placeholder="Your Email" tabindex="2" required="required" />
+		      	<input type="email" id="email" name="email" value="" placeholder="Your Email" tabindex="2" required="required"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
 		      </label>  
 			</div>
 		
@@ -49,7 +37,8 @@
 		   </form>
 
     </div>
+</div>
     
-<!-- test -->
-    </body>
-</html>
+<?php
+require_once "footer.php";
+?>
