3601b2bf56ced358e2629d4d53e999302ecaedc6
Birk Wertwein <BirkWertwein@Birks-MacBook-Air.local>
2020-04-11 12:13:34 +0200

    done with search functionality

--- search.php
+++ search.php
@@ -1,28 +1,51 @@
 <?php
 
-require_once "header.php";
 require_once "db.php";
 
 if(isset($_GET["q"])) {
     $sql = "SELECT * FROM products WHERE name LIKE CONCAT('%', :q, '%') OR description LIKE CONCAT('%', :q, '%')";
     // OR description LIKE %:q%"
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam(":q", $q);
+    $productStmt = $db->prepare($sql);
+    $productStmt->bindParam(":q", $q);
     $q = $_GET["q"];
-    $stmt->execute();
+    $productStmt->execute();
 
     $output = "<ul>";
 
-    while($row = $stmt->fetch(PDO::FETCH_ASSOC)):
-        $output .= "<li><h3>$row[name]</h3></li>";
-        $output .= "<li><p>$row[description]</p></li>";
+    while($productRows = $productStmt->fetch(PDO::FETCH_ASSOC)):
+        $sql = "SELECT * FROM product_images WHERE product_id = :id";
+        $imageStmt = $db->prepare($sql);
+        $imageStmt->bindParam(":id", $id);
+        $id = $productRows["id"];
+        $imageStmt->execute();
+        $imageRow = $imageStmt->fetch(PDO::FETCH_ASSOC);
+        // echo "<pre>";
+        // print_r($imageRow);
+        // echo "</pre>";
+        $image = $imageRow["image"];
+        
+        $output .= "<li><h3 class='title'>$productRows[name]</h3>";
+        $output .= "<span class='price'>$productRows[price]</span>";
+        $output .= "<img class='search-img' src='uploads/$image'></li>";
 
     endwhile;
 
     $output .= "</ul>";
-    echo $output;
 
 }
+
+require_once "header.php";
+
+?>
+
+<form class="search-form" action="search.php" method="GET">
+    <input class="search-page-input" type="text" name="q"/>
+    <button class="search-page-submit-btn" type="submit">Sök</button>
+</form>
+
+<?php 
+
+echo $output;
 
 require_once "footer.php";
 
