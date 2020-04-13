a3999eed1491c02abe6143e4d71c4240cfca8fc0
Angie Araya <angiearaya@hotmail.com>
2020-04-13 17:24:22 +0200

    added product api and product page

---
+++ product.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'header.php';
+
+$id = htmlspecialchars($_GET['id']);
+$url = "http://localhost/Webbshoppen/fetchProduct.php?id=" . $id;
+
+$json = file_get_contents($url);
+$jsonArr = json_decode($json, true);
+
+    $id = $jsonArr[0]['id'];
+    $name = $jsonArr[0]['name'];
+    $description = $jsonArr[0]['description'];
+    $price = $jsonArr[0]['price'];
+    $stock = $jsonArr[0]['stock'];
+    $img = $jsonArr[0]['images'];
+ 
+$productContainer = '<section>';
+
+foreach ($img as $key => $value) {
+    $productContainer .= "<div>
+                            <img src='$value' alt='' width='100' height='100' >
+                        </div>";
+}
+
+$length = count($jsonArr[0]['images']);
+
+$productContainer .= "<article>
+                        <h1>$name</h1>
+                        <p>$description</p>
+                        <p>$price Kr</p>
+                        <br>
+                        <p>$stock st finns i lager</p>
+                        <br>
+                    <input type='num' id='quantityInput'>
+                    <button id='' type='submit'>Lägg till i varukorg</button>
+                    </article></section>";    
+
+echo $productContainer;
+require_once 'footer.php';
+?>
+
