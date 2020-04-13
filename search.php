1301c9cbf19f7a0cb683b1057e93c51f2c364cf2
Birk Wertwein <BirkWertwein@Birks-MacBook-Air.local>
2020-04-13 13:44:46 +0200

    done with search functionality, ready for testing

--- search.php
+++ search.php
@@ -2,36 +2,25 @@
 
 require_once "db.php";
 
+$url = "http://localhost/Webbshoppen/api.php";
+$json = file_get_contents($url);
+$jsonArr = json_decode($json, true);
+
 if(isset($_GET["q"])) {
-    $sql = "SELECT * FROM products WHERE name LIKE CONCAT('%', :q, '%') OR description LIKE CONCAT('%', :q, '%')";
-    // OR description LIKE %:q%"
-    $productStmt = $db->prepare($sql);
-    $productStmt->bindParam(":q", $q);
-    $q = $_GET["q"];
-    $productStmt->execute();
 
-    $output = "<ul class='list'>";
+    $filtered = array_filter($jsonArr, function($v, $k) {
+        return strpos($v["name"], $_GET["q"]) || strpos($v["description"], $_GET["q"]);
+    }, ARRAY_FILTER_USE_BOTH);
 
-    while($productRows = $productStmt->fetch(PDO::FETCH_ASSOC)):
-        $sql = "SELECT * FROM product_images WHERE product_id = :id";
-        $imageStmt = $db->prepare($sql);
-        $imageStmt->bindParam(":id", $id);
-        $id = $productRows["id"];
-        $imageStmt->execute();
-        $imageRow = $imageStmt->fetch(PDO::FETCH_ASSOC);
-        // echo "<pre>";
-        // print_r($imageRow);
-        // echo "</pre>";
-        $image = $imageRow["image"];
-        
-        $output .= "<li class='list-item'><a href='product.php?id=$productRows[id]'><h3 class='title'>$productRows[name]</h3>";
-        $output .= "<img class='search-img' src='uploads/$image'>";
-        $output .= "<span class='price'>$productRows[price]:-</span></a></li>";
+    $output = "<ul class='list'>"; 
 
-    endwhile;
+    foreach($filtered as $value) {
+        $output .= "<li class='list-item'><a href='product.php?id=$value[id]'><h3 class='title'>$value[name]</h3>";
+        $output .= "<img class='search-img' src=" . $value["images"][0] . ">";
+        $output .= "<span class='price'>$value[price]:-</span></a></li>";
+    }
 
     $output .= "</ul>";
-
 }
 
 require_once "header.php";
