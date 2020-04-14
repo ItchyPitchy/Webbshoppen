8b1745e5af5bb994651c3d41ffb135b14de8bbe0
Vanessa Suthat <vanessasuthat@vanessas-mbp-2.mynet>
2020-04-14 15:18:48 +0200

    Limited the email input field and changed text to swedish.

--- contact.php
+++ contact.php
@@ -1,44 +1,46 @@
-<?php
 
-require_once "header.php";
-
-?>
 
 
 <div class="contactContainer">
 <div id="contact-form">
 	<div>
-		<h4 class="headerForm">Have a question or just want to get in touch?<br> Fyll out this form and let us know.</h4> 
+		<h4 class="headerForm">Har ni frågor eller funderingar?<br> Fyll i formuläret nedan så hör vi av oss så snart vi kan.</h4> 
 	</div>
 
 		   <form method="post" action="mailto:vanessasuthat@outlook.com" enctype="multipart/form-data">
 			<div>
 		      <label for="name">
-		      	<span class="required">Name: *</span> 
-		      	<input type="text" id="name" name="name" value="" placeholder="Your Name" required="required" tabindex="1" autofocus="autofocus" minlength="2" maxlength="20" />
+		      	<span class="required">Namn: *</span> 
+		      	<input type="text" id="name" name="name" value="" placeholder="Ditt namn" required="required" tabindex="1" autofocus="autofocus" minlength="2" maxlength="20" />
 		      </label> 
 			</div>
 			<div>
 		      <label for="email">
 		      	<span class="required">Email: *</span>
-		      	<input type="email" id="email" name="email" value="" placeholder="Your Email" tabindex="2" required="required"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
+				  <input type="email" 
+						   id="email" 
+						   name="email" 
+						   value="" 
+						   placeholder="Din e-postadress" 
+						   tabindex="2" required="required"  
+						   pattern="(?![^@]{30})[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*"
+						   oninvalid="this.setCostomValidity('Ogiltig e-postadress. Får max innehålla 30 tecken. T.ex. dittnamn@epost.se')"
+						   oninput ="this.setCustomValidity('')"
+						   title="Ogiltig e-postadress. Får max innehålla 30 tecken. T.ex. dittnamn@epost.se" />
 		      </label>  
 			</div>
 		
 			<div>		          
 		      <label for="message">
-		      	<span class="required">Message: *</span> 
-		      	<textarea id="message" name="message" placeholder="Please write your message here." tabindex="3" required="required"></textarea> 
+		      	<span class="required">Meddelande: *</span> 
+		      	<textarea id="message" name="message" placeholder="Ditt meddelande här." tabindex="3" required="required"></textarea> 
 		      </label>  
 			</div>
 			<div>		           
-		      <button name="submit" type="submit" id="submit" >SEND</button> 
+		      <button name="submit" type="submit" id="submit" >SKICKA</button> 
 			</div>
 		   </form>
 
     </div>
 </div>
     
-<?php
-require_once "footer.php";
-?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once "header.php";
 require_once 'db.php';
+require_once 'contact.php';
 require_once "footer.php";
 
 ?>
