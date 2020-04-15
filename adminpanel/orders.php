1ec92d908906ed49db0c735112d0f65d9fa9d1d1
Sofia Khan Yamanee <sofiakhan.y@hotmail.com>
2020-04-15 12:06:01 +0200

    HTML for Adminpanel (startpage)

---
+++ adminpanel/orders.php
@@ -0,0 +1,32 @@
+<div class="main-container">
+      <h2 class="admin_h2">Beställningar</h2>
+      <p class="filtrera-p">Filtrera beställningar</p>
+          <form class="search-form" action="" method="post">
+        <input class="search-input" type="text" />
+        <button class="search-submit-btn" type="submit">Sök</button>
+    </form>
+
+  
+    <div class="radio-div">
+    <input type="radio" id="alla" name="alla" checked="checked">
+    <label for="alla">Alla beställningar</label>
+    <input type="radio" id="aktiva" name="alla">
+    <label for="aktiva">Aktiva beställningar</label>
+    <input type="radio" id="slutförda" name="alla">
+    <label for="slutförda">Slutförda beställningar</label>
+    </div>
+
+<div class="width80">
+  <div class="dropdown">
+  <button class="mainmenubtn">Sortera efter ⇩</button>
+  <div class="dropdown-child">
+    <a href="">Beställnings datum</a>
+    <a href="">Ordersumma</a>
+    <a href="">Status</a>
+  </div>
+  </div>
+  </div>
+</div>
+  
+</div>
+
