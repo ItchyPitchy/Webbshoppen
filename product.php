6d86000e09073e75352eabac5c32ad8b41d3bd5b
Angie Araya <angiearaya@hotmail.com>
2020-04-15 22:59:25 +0200

    added css to product page

--- product.php
+++ product.php
@@ -23,24 +23,25 @@ for($i=0; $i < count($jsonArr); $i++) {
     $stock = $product['stock'];
     $img = $product['images'];
  
-$productContainer = "<main><section><div class='imgContainer'>";
+$productContainer = "<main><section class='productContainer1'><div class='imgContainer'>";
 
 foreach ($img as $key => $value) {
-    $productContainer .= "<div>
+    $productContainer .= "<div class='mySlides fade'>
                             <img src='$value' alt='Produkt bild'>
-                        </div>";
+                         </div>";
 }
+$productContainer .= "<a class='prev'>&#10094;</a>
+                      <a class='next'>&#10095;</a>";
 $productContainer .= '</div>';
 
-$productContainer .= "<article>
-                        <h1>$name</h1>
-                        <p>$description</p>
-                        <p>$price Kr</p>
+$productContainer .= "<article class='productInfo'>
+                        <h1 class='productName'>$name</h1>
+                        <p class='productInfo__description'>$description</p>
+                        <p class='productInfo__price'>$price Kr</p>
+                        <p class='productInfo__stock'>$stock st finns i lager</p>
+                        <input type='num' id='quantityInput'class='quantityInput' placeholder='ange antal'>
                         <br>
-                        <p>$stock st finns i lager</p>
-                        <br>
-                        <input type='num' id='quantityInput'>
-                        <button id='' type='submit'>Lägg till i varukorg</button>
+                        <button id='' type='submit'class='addToCartBtn'>Lägg till i varukorg</button>
                     </article>
                 </section>
             </main>";    
@@ -49,3 +50,31 @@ echo $productContainer;
 require_once 'footer.php';
 ?>
 
+<script>
+let prev = document.querySelector(".prev");
+prev.addEventListener("click", function(){
+    plusSlides(-1);
+});
+
+let next = document.querySelector(".next");
+next.addEventListener("click", function(){
+    plusSlides(1);
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+</script>
