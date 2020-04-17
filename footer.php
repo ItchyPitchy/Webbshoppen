082545e9b6767f68545e4e388df074c6cfcde46f
Frida <frida.johansson888gmail.com>
2020-04-17 08:51:13 +0200

    ändrat stavfel

--- footer.php
+++ footer.php
@@ -10,8 +10,8 @@
     <div class="header-category-links">
       <a class="header-category-link" href="http://localhost/Webbshoppen/category.php?category=light&kategori=Lampor">Lampor</a>
       <a class="header-category-link" href="http://localhost/Webbshoppen/category.php?category=table&kategori=Bord">Bord</a>
-      <a class="header-category-link" href="http://localhost/Webbshoppen/category.php?category=armchair&kategori=Fotöljer">Fotölj</a>
-      <a class="header-category-link" href="http://localhost/Webbshoppen/category.php?category=pillow&kategori=Kuddar">Kudde</a>
+      <a class="header-category-link" href="http://localhost/Webbshoppen/category.php?category=armchair&kategori=Fotöljer">Fåtöljer</a>
+      <a class="header-category-link" href="http://localhost/Webbshoppen/category.php?category=pillow&kategori=Kuddar">Kuddar</a>
       <a class="header-category-link" href="http://localhost/Webbshoppen/category.php?category=sofa&kategori=Soffor">Soffor</a>
     </div>
       <span class="powered_by">Powered by G04 2020</span>
