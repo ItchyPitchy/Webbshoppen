ba55931e21e9cb0e407af8bff01a3e2eb1c639d3
Frida <frida.johansson888gmail.com>
2020-04-25 11:06:13 +0200

    Added completed orders table

--- adminpanel/orders.php
+++ adminpanel/orders.php
@@ -36,6 +36,7 @@ require_once "header.php";
 
 <?php
 
+/***********************************************************Active orders table**************************************************************** */
 
 $orderContainer = '<div class="orderContainer">';
 
@@ -105,23 +106,24 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
     }
   }
 
-  if (sizeof($product_list)==0){
-    break;
-  }
   
   if ($status == 0) {
-    $statusform = "<form action='#' method='POST'>
+    $statusform = "<form action='orders.php?id=$active_orders_id' method='POST'>
     <select class='orderstatus-select' name='status'>
     <option name='option' selected='selected' value='0'>Ny</option>
     <option name='option' value='1'>Behandlas</option>
+    <option name='option' value='completed'>Slutförd</option>
+
     </select>
     <input type='submit' value='Ändra status'></form>";
   }
   else {
-    $statusform =  "<form action='#' method='POST'>
+    $statusform =  "<form action='orders.php?id=$active_orders_id' method='POST'>
     <select class='orderstatus-select' name='status'>
     <option name='option' value='0'>Ny</option>
     <option name='option' selected='selected' value='1'>Behandlas</option>
+    <option name='option' value='completed'>Slutförd</option>
+
     </select>
     <input type='submit' value='Ändra status'></form>";
   }
@@ -153,8 +155,110 @@ if ($result = $mysqli->query("SELECT * FROM active_orders LIMIT 1"))
 }
 $mysqli->close();
 
-$orderContainer .= "</div></div>";
-echo $orderContainer;     
+$orderContainer .= "</div>";
+echo $orderContainer;    
+
+
+/***********************************************************Completed orders table**************************************************************** */
+
+
+$completedOrderContainer = '<div class="orderContainer secondContainer">';
+
+echo "<h2 class='startpageHeading secondHeadingOrders'>Slutförda Beställningar</h2>";
+
+
+    $completedOrderContainer  .=  "<ul class='order-tr'>
+      <div class='column-div'><h3>Kund<h3></div>
+      <div class='column-div'><h3>Beställning<h3></div>
+      </ul>"; 
+ 
+
+$sql = "SELECT * FROM completed_orders";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+
+
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+
+
+
+  $products_id_list = [];
+  $quantity_list = []; 
+  $product_list = [];
+
+  $completed_orders_id = $row['completed_orders_id'];
+
+  $sum = $row['sum'];
+  $date = $row['date'];
+  $customers_id = $row['customers_id'];
+
+
+  $sql = "SELECT * FROM customers";
+  $stmt2 = $db->prepare($sql);
+  $stmt2->execute();
+
+  while($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)){
+
+    if ($customers_id == $row2['customer_id']) {
+      $name = $row2['name'];
+      $phone = $row2['phone'];
+      $street = $row2['street'];
+      $city = $row2['city'];
+
+      $sql = "SELECT * FROM completed_orders_products";
+      $stmt3 = $db->prepare($sql);
+      $stmt3->execute();
+
+      while($row3 = $stmt3->fetch(PDO::FETCH_ASSOC)){
+
+        if ($completed_orders_id == $row3['completed_orders_id']) {
+          array_push($products_id_list, $row3['products_id']);
+          array_push($quantity_list, $row3['quantity']);
+                    
+          $sql = "SELECT * FROM products";
+          $stmt4 = $db->prepare($sql);
+          $stmt4->execute();
+
+          while($row4 = $stmt4->fetch(PDO::FETCH_ASSOC)){
+            if ($row3['products_id'] == $row4['id']) {
+              array_push($product_list, $row4['name'], " : ", $row3['quantity'],"  st, <br>");
+
+            }        
+          } 
+        }
+      }
+    }
+  }
+
+  
+  $products = implode($product_list);
+    $completedOrderContainer  .=  "<ul class='order-tr'>
+      <div class='column-div'><li class='order-td'>$name</li>
+      <li class='order-td'>$phone</td>
+      <li class='order-td'>$street, $city </li></div>
+      <div class='column-div'><li class='order-td'>$date</li>
+      <li class='order-td'>$products</li>
+      <li class='order-td'>$sum:-</li></div>
+      </ul>"; 
+}
+
+
+
+$mysqli = new mysqli("localhost","root","","webshop");
+if ($result = $mysqli->query("SELECT * FROM completed_orders LIMIT 1"))
+{
+    if ($obj = $result->fetch_object())
+    {}
+    else
+    {
+    $completedOrderContainer .= "<br><br><Span>Det finns inga slutförda beställningar</span>";
+  }
+    $result->close();
+}
+$mysqli->close();
+
+$completedOrderContainer .= "</div></div>";
+echo $completedOrderContainer;  
 
 require_once "footer.php";
 ?>
