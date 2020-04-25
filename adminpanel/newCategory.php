fe7c1db28080e73ad266229d65712099641e3ecd
Vanessa Suthat <vanessasuthat@vanessas-mbp-2.mynet>
2020-04-26 00:23:04 +0200

    Added new category and validated the input.

---
+++ adminpanel/newCategory.php
@@ -0,0 +1,50 @@
+<?php
+require_once '../db.php';
+require_once 'header.php';
+$message = '';
+if (isset($_POST['save'])){
+
+    $insert_category = $_POST['category'];
+
+    $sql = "SELECT COUNT(*) AS num FROM category WHERE category = :category";
+
+            $stmt = $db->prepare($sql);
+            $stmt->bindValue(':category', $insert_category);
+            $stmt->execute();
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($row['num'] > 0){
+       
+        $output = "<div class='newCategoryMessage'>
+                        <p class='newCategoryMessage-text'>Denna kategori finns redan.</p>
+                        <a href='index.php'>OK</a>
+                        </div>";
+
+                echo $output;
+    }else{
+       
+        $sql ="INSERT INTO category (category)
+                SELECT '".$_POST["category"]."' FROM category
+                WHERE NOT EXISTS(
+                SELECT category FROM category WHERE category = '".$_POST["category"]."'
+                ) LIMIT 1
+                ";
+                    $stmt = $db->prepare($sql);
+                    $stmt->bindParam(':category', $category);
+                    $stmt->execute();
+
+                        $output = "<div class='newCategoryMessage'>
+                        <p class='newCategoryMessage-text'>Kategorin är tillagt.</p>
+                        <a href='index.php'>OK</a>
+                        </div>";
+                   
+                echo $output;
+    }
+      
+}
+?>       
+        
+<?php
+require_once 'footer.php';
+?>
