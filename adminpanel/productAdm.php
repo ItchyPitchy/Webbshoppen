a1e8c10c1432acddec271a182c6fd2f7b72bfdb4
Birk Zacharias Wertwein <birk.wertwein@gmail.com>
2020-04-28 21:14:04 +0200

    rewrite the code to prevent bug where product w/o images wouldnt show

--- adminpanel/productAdm.php
+++ adminpanel/productAdm.php
@@ -1,88 +1,61 @@
 <?php
+
 require_once '../db.php';
 require_once 'header.php';
 
-?>
+$output = "";
 
-<div class=" main-container adm-container">
-    <form action="#" method="POST" class="catForm">
-        <label for="category" class="textLabel">Redigera</label>
-        <input type="text" name="category" placeholder="Kategori" class="catInput">
-        <button type="submit" name="save" class="catSaveBtn">Spara</button>
-    </form>
-
-<?php
-$x = 0;
-if(isset($_GET['category_id'])){
+if (isset($_GET['category_id'])):
     
-    $category_id = htmlspecialchars($_GET['category_id']);
-
-    $sql = "SELECT * FROM products WHERE category_id = :category_id";
-    
+    $sql = "SELECT * FROM products WHERE category_id = :category_id AND deleted = 0";
     $stmt = $db->prepare($sql);
     $stmt->bindParam(':category_id', $category_id);
-    $stmt->execute(); 
+    $category_id = htmlspecialchars($_GET['category_id']);
+    $stmt->execute();
 
-}
-$link = "<a class='linkToNewProduct' href='newProduct.php?category_id=$category_id'>+</a>";
-
-$head = "<div class='productContainer'>
-            <table class='productTable'>
-                <thead>
-                    <tr>
-                        <th class='productHead'>Produkter $link </th>
-                    </tr>
-                </thead>";
-
- echo $head;
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-   
-        $sql ="SELECT * FROM product_images";
-        $stmt2 =$db->prepare($sql);
-        $stmt2 ->execute();
-
-        while($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)){
-            if($row['id'] == $row2['product_id']){
-                $name = htmlspecialchars($row['name']);
-                $id = $row['id'];
-                $img = "../images/" . $row2['image'];
-
-                $output ="<tr class='productContainer'>
-                            <td class='imgAdm'><img src=$img></td>
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)):
     
-                            <td class='admProductName'> $name </td>
-    
-                            <td class='admUpdateTd'>
-                                <a class='admUpdateBtn' href='updateProduct.php?id=$id' >Redigera </a>
-                            </td>
-    
-                            <td class='admDeleteTd'>
-                                <a class='admDeleteBtn' href='#' >  Radera </a>
-                            </td>
-                
-            </tr>";
-            $x++;
-            break;
-            }
-        
-        }
-        // echo '
-        // <div></div>
-        // ';
-        
-        
-         echo $output;
-}
+        $sql2 ="SELECT image FROM product_images WHERE product_id = :product_id";
+        $stmt2 = $db->prepare($sql2);
+        $stmt2->bindParam(":product_id", $product_id);
+        $product_id = htmlspecialchars($row["id"]);
+        $stmt2->execute();
+
+        $id = htmlspecialchars($row['id']);
+        $name = htmlspecialchars($row['name']);
+        $image = $stmt2->rowCount() ? $stmt2->fetch(PDO::FETCH_ASSOC)["image"] : "";
+        $url = "../images/$image";
+
+        $output .= "<tr class='productContainer'>
+                    <td class='imgAdm'><img src=$url></td>
+
+                    <td class='admProductName'>$name</td>
+
+                    <td class='admUpdateTd'>
+                        <a class='admUpdateBtn' href='updateProduct.php?id=$id'>Redigera </a>
+                    </td>
+
+                    <td class='admDeleteTd'>
+                        <a class='admDeleteBtn' href='#'>Radera</a>
+                    </td>
+                </tr>";
+    endwhile;
+
+
+endif;
 
 ?>
-</table>
+
+    <div class='productContainer'>
+        <table class='productTable'>
+            <thead>
+                <tr>
+                    <th class='productHead'>Produkter<a class='linkToNewProduct' href="newProduct.php?category_id=<?php echo $category_id; ?>">+</a></th>
+                </tr>
+            </thead>
+            <?php echo $output; ?>
+        </table>
+    </div>
 </div>
-</div>
 
-
-<?php
-require_once 'footer.php';
-?>
-
-
-
+<?php require_once 'footer.php'; ?>
