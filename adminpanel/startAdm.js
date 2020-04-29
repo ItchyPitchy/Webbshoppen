c8801cfd7f6c767991742f2274cd28dc3875e5a3
Birk Zacharias Wertwein <birk.wertwein@gmail.com>
2020-04-29 20:49:38 +0200

    changed max-limit to 20 chars

--- adminpanel/startAdm.js
+++ adminpanel/startAdm.js
@@ -2,14 +2,14 @@ const inputField = document.querySelector("#category-input");
 const lengthCounter = document.querySelector("#input-length-counter");
 
 if (lengthCounter && inputField) { 
-    lengthCounter.textContent = 50 - parseInt(inputField.value.length);
+    lengthCounter.textContent = 20 - parseInt(inputField.value.length);
     
     inputField.addEventListener("input", function(e) {
 
-        if (e.currentTarget.value.length > 50) {
-            e.currentTarget.value = e.currentTarget.value.substring(0, 50);
+        if (e.currentTarget.value.length > 20) {
+            e.currentTarget.value = e.currentTarget.value.substring(0, 20);
         }
-        lengthCounter.textContent = 50 - parseInt(e.currentTarget.value.length);
+        lengthCounter.textContent = 20 - parseInt(e.currentTarget.value.length);
     });
 
 }
@@ -24,7 +24,7 @@ function validateForm() {
         alerts[i].parentElement.removeChild(alerts[i]);
     }
 
-    if (input.trim().length === 0 || input.trim().length > 50) {
+    if (input.trim().length === 0 || input.trim().length > 20) {
         // const alert = document.createElement("span");
         // alert.classList.add("update-error");
         // alert.textContent = "upp till 50 tecken";
